8e9b3f8a9c66dd532c672ed58f31d43048faa8e6
Si Dao <si.dao@outlook.com>
2025-02-06 17:23:13 +0700

    Full bootable Linux with login enabled

--- flake.nix
+++ flake.nix
@@ -9,24 +9,8 @@
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
 
-      stage1 = pkgs.writeScript "init" ''
-        #!/bin/busybox sh
-
-        # expands the busybox
-        /bin/busybox --install
-
-        # make sure we have vfs mounted
-        /bin/busybox mount -t sysfs none /sys
-        /bin/busybox mount -t proc  none /proc
-
-        # generate device files
-        /bin/busybox mdev -s
-
-        HOME=/root USER=root exec /bin/busybox sh
-      '';
-
       kernel = "${pkgs.linuxPackages_latest.kernel}/bzImage";
-      initrd = pkgs.runCommand "build-initrd" { } ''
+      initrd = pkgs.runCommand "build-initrd" { buildInputs = [ pkgs.fakeroot ]; } ''
         mkdir tmp
 
         # generate the initrd
@@ -37,28 +21,69 @@
           closureInfo=${pkgs.closureInfo { rootPaths = pkgs.pkgsMusl.busybox; }}
           cp -Rf $(cat $closureInfo/store-paths) nix/store
 
-          closureInfo=${pkgs.closureInfo { rootPaths = stage1; }}
-          cp -Rf $(cat $closureInfo/store-paths) nix/store
+          cat <<< '#!/bin/sh
+          #!/bin/busybox sh
 
-          mkdir proc sys dev bin sbin
-          ln -s ${pkgs.pkgsMusl.busybox}/bin/busybox bin
+          mkdir /proc /sys /dev -p
 
-          find . | ${pkgs.cpio}/bin/cpio --quiet -H newc -o | gzip -9 -n > $out
+          # Mount essential filesystems
+          mount -t proc none     /proc
+          mount -t sysfs none    /sys
+          mount -t devtmpfs none /dev
+
+          mkdir -p /dev/pts /dev/shm
+          mount -t tmpfs tmpfs   /dev/shm
+          mount -t devpts devpts /dev/pts
+
+	  chown root:tty /dev/tty /dev/tty[1-4]
+          chmod 620 /dev/tty[1-4]
+
+          # Switch to the real init
+          exec /sbin/init
+          ' > init
+          chmod +x init
+
+          mkdir -p etc/init.d
+          cat <<< '#!/bin/sh
+          #!/bin/sh
+
+          # Set hostname
+          echo "anchi-linux" > /proc/sys/kernel/hostname
+
+          echo "Welcome to Anchi Linux"
+          ' > etc/init.d/rcS
+          chmod +x etc/init.d/rcS
+
+          echo '::sysinit:/etc/init.d/rcS'                 >> etc/inittab
+          echo '::respawn:/sbin/getty -L ttyS0 9600 vt100' >> etc/inittab
+	  echo 'tty1::respawn:/sbin/getty 38400 tty1'      >> etc/inittab
+          echo '::ctrlaltdel:/sbin/reboot'                 >> etc/inittab
+
+          echo 'root::0:0:root:/root:/bin/sh'              >> etc/passwd
+          echo 'user::1000:100:user:/home/user:/bin/sh'    >> etc/passwd
+	  echo "tty:x:5:"                                  >> etc/group
+	  echo "root:x:0:"                                 >> etc/group
+	  echo "users:x:100:"                              >> etc/group
+
+	  chmod 644 etc/passwd etc/group
+
+	  echo "PS1='[\u@\h \w]\$ '"                       >> etc/profile
+
+          ln -s ${pkgs.pkgsMusl.busybox}/{bin,sbin,linuxrc} .
+
+          # to be able to have correct permission
+          fakeroot sh -c "find . | ${pkgs.cpio}/bin/cpio --quiet -H newc -o | gzip -9 -n > $out"
         )
       '';
 
       rootfs = "/tmp/rootfs.img";
 
-      runvm = pkgs.writeScriptBin "runvm" ''
-        #!${pkgs.stdenv.shell}
-
-        ${pkgs.closureInfo { rootPaths = pkgs.dinit; }}
-
+      runvm = pkgs.writeShellScriptBin "runvm" ''
         [ -f "${rootfs}" ] || trunscate -s 1G ${rootfs}
         ${pkgs.qemu_kvm}/bin/qemu-kvm -nographic -name singoc \
-          -m 2048 -smp 4 -kernel ${kernel} -initrd ${initrd} -drive \
-      	format=raw,file=${rootfs} \
-      	-append 'root=/dev/ram rdinit=${stage1} console=ttyS0 loglevel=4'
+          -m 2048 -smp 4 -kernel ${kernel} -initrd ${initrd} \
+          -drive format=raw,file=${rootfs} \
+          -append 'root=/dev/ram rdinit=/init console=ttyS0,115200 loglevel=4'
       '';
     in
     {
