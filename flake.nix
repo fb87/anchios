16daf71811c61b041485107bcc01be1d4e44b6a9
Si Dao <si.dao@outlook.com>
2025-02-08 11:44:34 +0700

    Add some utils such as openssh, vim

--- flake.nix
+++ flake.nix
@@ -20,7 +20,9 @@
 
           # migrate the store to new rootfs
           closureInfo=${pkgs.closureInfo { rootPaths = with pkgs.pkgsMusl; [
-            busybox vim fastfetch
+            busybox vim openssh
+          ] ++ [
+            pkgs.linuxPackages_latest.kernel
           ]; }};
 
           cp -Rf $(cat $closureInfo/store-paths) nix/store
@@ -31,15 +33,15 @@
           mkdir /proc /sys /dev -p
 
           # Mount essential filesystems
-          mount -t proc none     /proc
-          mount -t sysfs none    /sys
-          mount -t devtmpfs none /dev
+          mount -t proc none         /proc
+          mount -t sysfs none        /sys
+          mount -t devtmpfs none     /dev
 
           mkdir -p /dev/pts /dev/shm
-          mount -t tmpfs tmpfs   /dev/shm
-          mount -t devpts devpts /dev/pts
+          mount -t tmpfs tmpfs       /dev/shm
+          mount -t devpts devpts     /dev/pts
 
-          chown root:tty /dev/tty /dev/tty[1-4]
+          chown root:tty /dev/tty    /dev/tty[1-4]
           chmod 620 /dev/tty[1-4]
 
           # Switch to the real init
@@ -54,6 +56,13 @@
           # Set hostname
           echo "anchi-linux" > /proc/sys/kernel/hostname
 
+          [ -e /sys/class/net/eth0 ] || modprobe e1000
+          [ -e /sys/class/net/eth0 ] && udhcpc -i eth0
+
+          mkdir -p /var/empty
+          ${pkgs.pkgsMusl.openssh}/bin/ssh-keygen -A
+          ${pkgs.pkgsMusl.openssh}/bin/sshd -D &
+
           echo "Welcome to Anchi Linux"
           ' > etc/init.d/rcS
           chmod +x etc/init.d/rcS
@@ -64,29 +73,55 @@
           echo '::ctrlaltdel:/sbin/reboot'                 >> etc/inittab
 
           echo 'root::0:0:root:/root:/bin/sh'              >> etc/passwd
-          echo 'user::1000:100:user:/home/user:/bin/sh'    >> etc/passwd
-          echo "tty:x:5:"                                  >> etc/group
+          echo 'sshd:x:74:74:::'                           >> etc/passwd
+          echo 'user:x:1000:100:user:/home/user:/bin/sh'   >> etc/passwd
+
+          echo "tty:x:5:user"                              >> etc/group
           echo "root:x:0:"                                 >> etc/group
-          echo "users:x:100:"                              >> etc/group
+          echo "sshd:x:74:"                                >> etc/group
+          echo "users:x:100:user"                          >> etc/group
+
+          echo "root::0:0:99999:7:::"                      >> etc/shadow
+          hashed=$(${pkgs.openssl}/bin/openssl passwd -6 -salt anchios user)
+          echo "user:$hashed:90:0:99999:7:::"              >> etc/shadow
 
           chmod 644 etc/passwd etc/group
+          chmod 640 etc/shadow
 
           echo "PS1='[\u@\h \w]\$ '"                       >> etc/profile
 
           ln -s ${pkgs.pkgsMusl.busybox}/{bin,sbin,linuxrc} .
+
+          mkdir -p etc/ssh
+          cat <<< '
+          PermitEmptyPasswords   yes
+          Port                   22
+          PermitRootLogin        yes
+          PasswordAuthentication yes
+          ' > etc/ssh/sshd_config
+
+          mkdir -p lib
+          ln -s ${pkgs.linuxPackages_latest.kernel}/lib/modules lib
+
+          mkdir -p home/user && chown 1000:100 -R home/user
 
           # to be able to have correct permission
           fakeroot sh -c "find . | ${pkgs.cpio}/bin/cpio --quiet -H newc -o | gzip -9 -n > $out"
         )
       '';
 
+      iso = pkgs.runCommandNoCC "gen-iso" ''
+      '';
+
       rootfs = "/tmp/rootfs.img";
 
       runvm = pkgs.writeShellScriptBin "runvm" ''
-        [ -f "${rootfs}" ] || trunscate -s 1G ${rootfs}
+        [ -f "${rootfs}" ] || ${pkgs.qemu_kvm}/bin/qemu-img create -f raw ${rootfs} 2G
         ${pkgs.qemu_kvm}/bin/qemu-kvm -nographic -name singoc \
           -m 2048 -smp 4 -kernel ${kernel} -initrd ${initrd} \
           -drive format=raw,file=${rootfs} \
+          -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+          -device e1000,netdev=net0 \
           -append 'root=/dev/ram rdinit=/init console=ttyS0,115200 loglevel=4'
       '';
     in
